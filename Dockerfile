170459e74302d9a9b0203eaa401ab77cdb660114
张雷 <zhanglei@bonree.com>
2025-05-29 09:54:25 +0900

    添加样例页面

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,8 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+# COPY . .
+EXPOSE 8081
+
+# 默认命令 (会被 docker-compose.yml 中的 command 覆盖)
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8081"]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
+import datetime
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    """
+    Renders the homepage.
+    """
+    context = {
+        'page_title': 'Welcome to My Django Homepage-docker',
+        'message': 'Hello from Django!',
+        # Add current time
+        'current_time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z"),
+    }
+    return render(request, 'home/index.html', context)
